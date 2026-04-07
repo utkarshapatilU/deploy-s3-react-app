f9103eee72d65199683e632ef92d5324d417f128
Utkarsha patil <patilur010@gmail.com>
2026-04-07 16:06:05 +0530

    updated files

---
+++ s3-react-demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React CI/CD Deployment</h1>
+      <p>Version 1 - Initial Deployment</p>
+    </div>
+  );
+}
+
+export default App;
